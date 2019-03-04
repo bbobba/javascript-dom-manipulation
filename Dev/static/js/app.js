671c2cde92259d42c23d2822234fd72d4bf2e4cc
Becky Bobba <becky.h.bobba@gmail.com>
2019-03-04 09:50:06 -0600

    renamed startercode file to dev

---
+++ Dev/static/js/app.js
@@ -0,0 +1,9 @@
+// TIPS & RESOURCES:
+// @TODO: Delete this section for final solution
+
+// -------------------------------------------------------
+
+// from data.js
+var tableData = data;
+
+// YOUR CODE HERE!
